5ef98bacb93a497c01f3136234852d9ddfa156ee
iwakefield <dominicwakefield11@gmail.com>
2022-10-21 20:47:35 -0700

    debugging task completed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
-
 /**
  * main - prints if integer is positive or negative
- *
- * Return: 0
+ *Return: 0
  */
 
 int main(void)
@@ -12,7 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
