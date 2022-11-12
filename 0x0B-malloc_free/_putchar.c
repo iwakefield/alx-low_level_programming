4b00416e107931ef977365b3464bbf1452ee6b26
iwakefield <dominicwakefield11@gmail.com>
2022-11-11 19:28:44 -0800

    task completed

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to be printed
+ * Return: 1(Success)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
